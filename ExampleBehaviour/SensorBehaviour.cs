af31e6aa4010e47c3a8b24bdaf48bfcff37dac67
Кудрявский Роман <devpilgrim@gmail.com>
2017-10-04 10:56:00 +0300

    Update SensorBehaviour.cs

--- ExampleBehaviour/SensorBehaviour.cs
+++ ExampleBehaviour/SensorBehaviour.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
-using System.Collections;
 using System;
-using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class SensorBehaviour : MonoBehaviour
 {
 	private AndroidJavaObject plugin;
 
+    /// <summary>
+    /// Сенсор
+    /// </summary>
+    public SensorName Sensor;
+
+
 	void Start ()
 	{
 #if UNITY_ANDROID
+        //Подключаем плагин сенсора
 		plugin = new AndroidJavaClass("jp.kshoji.unity.sensor.UnitySensorPlugin").CallStatic<AndroidJavaObject>("getInstance");
-		plugin.Call("setSamplingPeriod", 100 * 1000); // refresh sensor 100 mSec each
-		plugin.Call("startSensorListening", "accelerometer");
+        //Задаем параметры обновления сенсора в милисекундах
+		plugin.Call("setSamplingPeriod", 100 * 1000);
+        //Активируем сенсор
+        ActivateSensor(Sensor);
 #endif
 	}
 
+    /// <summary>
+    /// При выходе из приложения отключить сенсоры
+    /// </summary>
 	void OnApplicationQuit ()
 	{
 #if UNITY_ANDROID
@@ -28,13 +39,64 @@ public class SensorBehaviour : MonoBehaviour
 
 	void Update ()
 	{
-#if UNITY_ANDROID
-		if (plugin != null) {
-			float[] sensorValue = plugin.Call<float[]>("getSensorValues", "accelerometer");
-			if (sensorValue != null) {
-				Debug.Log("sensorValue:" + string.Join(",", new List<float>(sensorValue).ConvertAll(i => i.ToString()).ToArray()));
-			}
-		}
-#endif
+	    string s = "";
+
+	    foreach (float f in Get_Sensor(Sensor))
+	    {
+	        s += f + " :: ";
+	    }
+        Debug.Log(s);
 	}
+
+    /// <summary>
+    /// Получить показания сенсора
+    /// </summary>
+    /// <param name="sensor">Тип сенсора</param>
+    /// <returns>Массив значений показаний сенсора</returns>
+    public float[] Get_Sensor(SensorName sensor)
+    {
+#if UNITY_ANDROID
+        if (plugin != null)
+        {
+            return plugin.Call<float[]>("getSensorValues", Enum.GetName(typeof(SensorName), sensor));
+        }
+#endif
+        return null;
+    }
+
+    /// <summary>
+    /// Активация сенсора.
+    /// </summary>
+    /// <param name="sensor"></param>
+    public void ActivateSensor(SensorName sensor)
+    {
+#if UNITY_ANDROID
+        plugin.Call("startSensorListening", Enum.GetName(typeof(SensorName), sensor));
+#endif
+    }
+
+    /// <summary>
+    /// Тип сенсора
+    /// </summary>
+    public enum SensorName {
+        accelerometer,
+        ambienttemperature,
+        gamerotationvector,
+        geomagneticrotationvector,
+        gravity,
+        gyroscope,
+        gyroscopeuncalibrated,
+        heartrate,
+        light,
+        linearacceleration,
+        magneticfield,
+        magneticfielduncalibrated,
+        pressure,
+        proximity,
+        relativehumidity,
+        rotationvector,
+        significantmotion,
+        stepcounter,
+        stepdetector,
+    };
 }
